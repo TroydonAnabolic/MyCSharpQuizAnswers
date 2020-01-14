422f56236283bd4220cf9302ef2e3a37f284b035
Troydon Luicien <troyincarnate@gmail.com>
2020-01-14 17:13:56 +1100

    58. Write a C# program which will accept a list of integers and checks how many integers are needed to complete the range. Go to the editor Sample Example [1, 3, 4, 7, 9], between 1-9 -> 2, 5, 6, 8 are not present in the list. So output will be 4. Click me to see the solution

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,8 @@
 ﻿/*
  
- 57.Write a C# program to find the pair of adjacent elements that has the highest product of an given array of integers
+58. Write a C# program which will accept a list of integers and checks how many integers are needed to complete the range. Go to the editor
+Sample Example [1, 3, 4, 7, 9], between 1-9 -> 2, 5, 6, 8 are not present in the list. So output will be 4.
+Click me to see the solution
 
  */
 using System;
@@ -14,30 +16,36 @@ class Test
 
     public static void Main()
     {
-        int[] intArray = { 6, 1, 12, 3, 1, 4, 20};
-        Console.WriteLine($"Enter a value to check if the string is a palindrome");
+        int[] intArray = { 1, 3, 4, 7, 9 };
 
-        Console.WriteLine($"The highest product is {HighestIntProduct(intArray)}");
+        Console.WriteLine($"The number of integers missing to complete the range is: {MissingInteger(intArray)}");
     }
 
-    private static int HighestIntProduct(int[] intArray)
+    private static int MissingInteger(int[] intArray)
     {
-        // initial product we test is the first and second
-        int i = 0;
-        // the first product value in the variable is the first 2 which is 6 * 1 = 6
-        int product = intArray[i] * intArray[i + 1] ;
-        // move i to element 2
-        i++;
+        int i = 0, intToAdd = 0,  difference ;
 
         // check all elements except the last element, to avoid out of bounds
         while (i + 1 < intArray.Length)
         {
-            // product is assigned the value if the checked instance product is more than current store value
-            product = (intArray[i] * intArray[i + 1]) > product ? // is 1*12 > 6
-                      intArray[i] * intArray[i + 1] : // if true then keep iterating and assign product to this instance
-                      product; // otherwise let product remain product
+            // check if the element value + 1, is equal to the value of the next element
+            if((intArray[i] + 1).Equals(intArray[i + 1])) // iteration 1: intArray[0] + 1 != intArray[1] so follow else: 3 - 1 = 2 - 1
+            {
+                // if it is then add 0 to the value of intToAdd
+                difference = 0;
+            }
+            else
+            {
+                // if the difference is greater than 2, then that is the number of elements missing, otherwise it is one element missing
+                difference = ((intArray[i + 1] - intArray[i]) > 2) ?
+                             (intArray[i + 1] - intArray[i]) - 1 :
+                               1;
+
+            }
+            // increase the value of intToAdd, by the amount of difference
+            intToAdd += difference; 
             i++;
         }
-        return product;
+        return intToAdd;
     }
 }
